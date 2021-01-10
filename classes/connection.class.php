fef257ff484c8b6fe67cb589643108d54e84836d
Rihards Mednis <mednisrihards@gmail.com>
2021-01-10 19:29:42 +0200

    set php to use dotenv variables

--- classes/connection.class.php
+++ classes/connection.class.php
@@ -1,17 +1,25 @@
 <?php
 
+require_once __DIR__ . '/../vendor/autoload.php';
+$dotenv = \Dotenv\Dotenv::createImmutable(__DIR__.'/../');
+$dotenv->load();
+
 class Connection {
-    //CONNECTION TO DATABASE
-    private $host = "localhost";
-    private $user = "RihardsMednisCom";
-    private $pw = "RihardsMednisCom";
-    private $dbname = "products";
-    
-    protected function connect(){
+    private $host;
+    private $user;
+    private $pw;
+    private $dbname;
+
+    function __construct(){
+        $this->host = $_ENV['MYSQL_HOST'];
+        $this->user = $_ENV['MYSQL_USER'] ;
+        $this->pw = $_ENV['MYSQL_PASSWORD'] ;
+        $this->dbname = $_ENV['MYSQL_DB'] ;
+    }
+
+    protected function connect() {
             $connection = new PDO("mysql:host=$this->host;dbname=$this->dbname", $this->user, $this->pw);    
         return $connection;
    
     }
 }
-    
-    
